b2c381db43b1940a399111eb896a94d5f499599e
hiitsu <jarkko.hiitoluoma@gmail.com>
2020-01-05 20:40:26 +0200

    test: shape routines test now if arrays are actually cloned

--- src/shape.test.ts
+++ src/shape.test.ts
@@ -8,41 +8,60 @@ import {
   sizeOf
 } from "./shape";
 
-const shape: Shape = [
-  [0, 0, 0, 0, 0],
-  [1, 2, 3, 4, 6]
-];
-
 describe("Shape", () => {
-  const shape: Shape = [
-    [0, 0, 1],
-    [1, 1, 1]
-  ];
+  let shape: Shape;
+  beforeEach(() => {
+    shape = [
+      [0, 0, 1],
+      [1, 1, 1]
+    ];
+  });
 
   it("sizeOf", () => {
     expect(sizeOf(shape)).toBe(4);
   });
 
-  it("flipX", () => {
-    expect(flipX(shape)).toStrictEqual([
-      [1, 0, 0],
-      [1, 1, 1]
-    ]);
+  describe("flipX", () => {
+    it("flips correct values", () => {
+      expect(flipX(shape)).toStrictEqual([
+        [1, 0, 0],
+        [1, 1, 1]
+      ]);
+    });
+    it("clones arrays deeply", () => {
+      const flipped = flipX(shape);
+      expect(flipped).not.toBe(shape);
+      expect(flipped[0]).not.toBe(shape[0]);
+    });
   });
 
-  it("flipY", () => {
-    expect(flipY(shape)).toStrictEqual([
-      [1, 1, 1],
-      [0, 0, 1]
-    ]);
+  describe("flipY", () => {
+    it("flips correct values", () => {
+      expect(flipY(shape)).toStrictEqual([
+        [1, 1, 1],
+        [0, 0, 1]
+      ]);
+    });
+    it("clones arrays deeply", () => {
+      const flipped = flipY(shape);
+      expect(flipped).not.toBe(shape);
+      expect(flipped[0]).not.toBe(shape[0]);
+    });
   });
 
-  it("rotateClockWise90", () => {
-    expect(rotateClockWise90(shape)).toStrictEqual([
-      [1, 0],
-      [1, 0],
-      [1, 1]
-    ]);
+  describe("rotateClockWise90", () => {
+    it("rotates correct values", () => {
+      expect(rotateClockWise90(shape)).toStrictEqual([
+        [1, 0],
+        [1, 0],
+        [1, 1]
+      ]);
+    });
+    it("clones arrays deeply", () => {
+      const rotated = rotateClockWise90(shape);
+      expect(rotated).not.toBe(shape);
+      expect(rotated[0]).not.toBe(shape[0]);
+    });
   });
 
   it("shapeVariations", () => {
