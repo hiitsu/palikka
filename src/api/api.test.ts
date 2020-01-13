2ce1a5d13d39a103cf82c47db42395398b4e99de
hiitsu <jarkko.hiitoluoma@gmail.com>
2020-01-13 15:05:01 +0200

    feat: some basic jwt signig and verification

--- src/api/api.test.ts
+++ src/api/api.test.ts
@@ -4,9 +4,11 @@ import buildFastify from "./api";
 
 describe("api", () => {
   let fastify: fastify.FastifyInstance<http.Server, http.IncomingMessage, http.ServerResponse>;
+
   beforeAll(() => {
     fastify = buildFastify();
   });
+
   afterAll(() => {
     fastify.close();
   });
@@ -30,5 +32,27 @@ describe("api", () => {
     expect(response.headers["X-Download-Options".toLocaleLowerCase()]).toBeTruthy();
     expect(response.headers["X-Content-Type-Options".toLocaleLowerCase()]).toBeTruthy();
     expect(response.headers["X-XSS-Protection".toLocaleLowerCase()]).toBeTruthy();
+  });
+
+  describe("signup", () => {
+    it("should give a token", async () => {
+      const response = await fastify.inject({
+        method: "POST",
+        url: "/signup"
+      });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.payload).toBeTruthy();
+    });
+  });
+
+  describe("score", () => {
+    it("should give 401 is sending without token", async () => {
+      const response = await fastify.inject({
+        method: "POST",
+        url: "/score"
+      });
+      expect(response.statusCode).toBe(401);
+    });
   });
 });
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,35 +1,60 @@
-import fastify from "fastify";
+import fastify, { JWT } from "fastify";
 import http from "http";
 import helmet from "fastify-helmet";
+import jwt from "fastify-jwt";
 
 export default function() {
   const server = fastify();
 
   server.register(helmet);
+  server.register(jwt, {
+    secret: "supersecret"
+  });
 
-  const opts = {
-    schema: {
-      response: {
-        200: {
-          type: "object",
-          properties: {
-            puzzle: {
-              type: "string"
+  async function onRequest(request: any, reply: any) {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      reply.send(err);
+    }
+  }
+
+  server.post("/signup", (_request: any, reply: any) => {
+    const payload = "abc";
+    const token = (server.jwt as JWT).sign({ payload });
+    reply.send({ token });
+  });
+
+  server.post(
+    "/score",
+    { onRequest },
+    (_req: fastify.FastifyRequest<http.IncomingMessage>, reply: fastify.FastifyReply<http.ServerResponse>) => {
+      reply.header("Content-Type", "application/json").code(200);
+      reply.send({ score: 1 });
+    }
+  );
+
+  server.get(
+    "/",
+    {
+      schema: {
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              puzzle: {
+                type: "string"
+              }
             }
           }
         }
       }
+    },
+    (_req: fastify.FastifyRequest<http.IncomingMessage>, reply: fastify.FastifyReply<http.ServerResponse>) => {
+      reply.header("Content-Type", "application/json").code(200);
+      reply.send({ puzzle: "time" });
     }
-  };
+  );
 
-  function handler(
-    _req: fastify.FastifyRequest<http.IncomingMessage>,
-    reply: fastify.FastifyReply<http.ServerResponse>
-  ) {
-    reply.header("Content-Type", "application/json").code(200);
-    reply.send({ puzzle: "time", abc: 1 });
-  }
-
-  server.get("/", opts, handler);
   return server;
 }
