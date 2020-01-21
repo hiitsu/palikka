8c54b21b8ff562ef7d438299ad15fcde79fd2b79
hiitsu <jarkko.hiitoluoma@gmail.com>
2020-01-21 12:35:50 +0200

    test: test json and int[][] db operations in knex.test

--- src/api/knex.test.ts
+++ src/api/knex.test.ts
@@ -1,19 +1,51 @@
 import knex, { destroy } from "./knex";
 
 describe("database", () => {
-  beforeAll(async () => {
-    await knex("puzzles").del();
-  });
+  function swallow() {}
+  async function cleanup() {
+    await knex("jsontest")
+      .del()
+      .catch(swallow);
+    await knex.schema.dropTable("jsontest").catch(swallow);
+    await knex("arraytest")
+      .del()
+      .catch(swallow);
+    await knex.schema.dropTable("arraytest").catch(swallow);
+  }
+  beforeAll(cleanup);
 
   afterAll(async () => {
-    await knex("puzzles").del();
+    await cleanup();
     await destroy();
   });
 
-  it("read and write", async () => {
-    await knex("puzzles").insert({ blocks: JSON.stringify([1, 2, 3]) });
-    const rows = await knex("puzzles").select("blocks");
-    expect(rows).toBeDefined();
+  it("create schema with json and read and write", async () => {
+    await knex.schema.createTable("jsontest", function(table) {
+      table.increments("id").notNullable();
+      table.json("blocks");
+    });
+    await knex("jsontest").insert({ blocks: JSON.stringify([1, 2, 3]) });
+    const rows = await knex("jsontest").select("blocks");
     expect(rows).toHaveLength(1);
+    expect(typeof rows[0].blocks).toBe("object");
+  });
+
+  it("create schema with int[][] and write and read it", async () => {
+    await knex.schema.createTable("arraytest", function(table) {
+      table.increments("id").notNullable();
+      table.specificType("grid", "INT[][]");
+    });
+    await knex("arraytest").insert({
+      grid: [
+        [1, 1],
+        [0, 1]
+      ]
+    });
+    const rows = await knex("arraytest").select("*");
+    expect(rows).toHaveLength(1);
+    expect(rows[0].grid).toStrictEqual([
+      [1, 1],
+      [0, 1]
+    ]);
   });
 });
