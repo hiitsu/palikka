619318fc63e489cae55c6d24b6f2b8611fb07d26
hiitsu <jarkko.hiitoluoma@gmail.com>
2020-02-07 10:56:10 +0200

    fix(ui): CommonHead now sets font before loading to have better default font before its loaded

--- src/ui/CommonHead.tsx
+++ src/ui/CommonHead.tsx
@@ -4,16 +4,6 @@ export default function CommonHead(props: { title: string }) {
   return (
     <Head>
       <title>{props.title}</title>
-      <meta
-        name="viewport"
-        content="minimal-ui, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width, user-scalable=no"
-        key="viewport"
-      />
-      <link
-        key="google-fonts"
-        href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap"
-        rel="stylesheet"
-      />
       <style>{`
         body,
         html {
@@ -150,6 +140,17 @@ export default function CommonHead(props: { title: string }) {
           letter-spacing: -1px;
         }
       `}</style>
+      <meta
+        name="viewport"
+        content="minimal-ui, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width, user-scalable=no"
+        key="viewport"
+      />
+      <link
+        key="google-fonts"
+        href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap"
+        rel="stylesheet"
+      />
+
       <meta key="msapplication-tap-highlight" name="msapplication-tap-highlight" content="no" />
     </Head>
   );
